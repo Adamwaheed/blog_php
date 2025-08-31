4b9279b9c068d650b828bf8c95b1c6e87949c65a
Shiyam <abd.shiyam@gmail.com>
2025-08-31 21:47:31 +0500

    Update BASE_URL in config.php for correct local development path

--- core/config.php
+++ core/config.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_URL', 'http://localhost:7090');
+define('BASE_URL', 'http://localhost:8080/blog_php-1');
 
 function redirect($path = '') {
     $url = BASE_URL . '/' . ltrim($path, '/');
